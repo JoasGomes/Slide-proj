126882fcbab8f2ffa5a001586c62d0a092023602
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-25 21:23:15 -0300

    navegação prev e next

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,4 @@
-export default class Slide {
+export class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
@@ -71,6 +71,9 @@ export default class Slide {
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
 
+        this.activePrevSlide = this.activePrevSlide.bind(this);
+        this.activeNextSlide = this.activeNextSlide.bind(this);
+
         this.onResize = this.onResize.bind(this);
     }
 
@@ -136,6 +139,20 @@ export default class Slide {
         this.addSlideEvents();
         this.slidesConfig();
         this.addResizeEvent();
+        this.changeSlide(2);
         return this;
+    }
+}
+
+export class SlideNav extends Slide {
+    addArrow(prev, next) {
+        this.prevElement = document.querySelector(prev);
+        this.nextElement = document.querySelector(next);
+        this.addArrowEvent();
+    }
+
+    addArrowEvent() {
+        this.prevElement.addEventListener('click', this.activePrevSlide);
+        this.nextElement.addEventListener('click', this.activeNextSlide);
     }
 }
