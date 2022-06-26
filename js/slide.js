b24c8019bac66cf273063a4c499bfda3709c6fb2
JoasGomes <jojoca.vitor777@gmail.com>
2022-06-25 21:04:25 -0300

    resize

--- js/slide.js
+++ js/slide.js
@@ -3,6 +3,7 @@ export default class Slide {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
         this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+        this.activeClass = 'active';
     }
 
     transition(active) {
@@ -69,6 +70,8 @@ export default class Slide {
         this.onStart = this.onStart.bind(this);
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
+
+        this.onResize = this.onResize.bind(this);
     }
 
     //slides config
@@ -99,6 +102,12 @@ export default class Slide {
         this.moveSlide(activeSlide.position);
         this.slidesIndexNav(index);
         this.dist.finalPosition = activeSlide.position;
+        this.changeActiveClass();
+    }
+
+    changeActiveClass() {
+        this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+        this.slideArray[this.index.active].element.classList.add(this.activeClass);
     }
 
     activePrevSlide() {
@@ -109,11 +118,24 @@ export default class Slide {
         if(this.index.next !== undefined) this.changeSlide(this.index.next);
     }
 
+    onResize() {
+        setTimeout(() => {
+            this.slidesConfig();
+            this.changeSlide(this.index.active);
+        }, 1000);
+        
+    }
+
+    addResizeEvent() {
+        window.addEventListener('resize', this.onResize);
+    }
+
     init() {
         this.bindEvents();
         this.transition(true);
         this.addSlideEvents();
         this.slidesConfig();
+        this.addResizeEvent();
         return this;
     }
 }
